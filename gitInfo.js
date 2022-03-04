395e3e16257dcd2be6745c4884ea96ee5f9bec2c
Adam Mackey <adambmackey@gmail.com>
2022-03-04 11:32:31 -0500

    gitinfo = done

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+var gitDefinition =`Git is a repository langage that allows you to take a snapshot or copy of your code and store it on your local computer`
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+var gitHubDefinition =`GitHub is a website owned by Microsoft that lets anyone upload and store their code for free. you can use git on your local computer to "push" your code to GitHub where it will not get lost, and you will always have copies of it. you can push as many copies as you want`
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+var gitInitDefinition = `git init is the first step to setting up a local repo on your computer. it initilizes the repository, preparing it to hold and save your directory of coding files`
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+var gitCloneDefinition = `git clone is used to point to an existing repo and make a clone or copy of that repo in a new directory, at another location.`
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+var gitStatusDefinition = `git status is used to see where you are at in the process of saving your code to your local repo. it can tell you if you have added and or commited the updated files letting you know which changes have been staged`
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,9 @@
 */
 
 //CODE HERE
+var gitAddDefinition = `The git add command adds a change in the working directory to the staging area. It tells Git that you want to include updates to a particular file in the next commit.`
 
+var gitAddCode = `git add .`
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,8 +66,9 @@
 */
 
 //CODE HERE
+var gitCommitDefinition = `git commit captues a snapshot of the projects currently staged changes.snapshots can be thought of as "safe" versions of a project`
 
-
+var gitCommitCode = `git commit -m 'initial commit'`
 //////////////////PROBLEM 8////////////////////
 /*
     Create a variable called 'gitPushDefinition'.  
@@ -73,3 +76,4 @@
 */
 
 //CODE HERE
+var gitPushDefinition = `git push takes the local repo and sends it to git hub using the url to sync at github and stores a copy of your files on git hub`
